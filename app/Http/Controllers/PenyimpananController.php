a8c672eb2d0e03785c5f468e081b85a6357f9752
Muhamad Abdul Rosyid <pepey@MacBook-Air-Muhamad.local>
2024-09-08 16:40:59 +0700

    Tambahan UAS

--- app/Http/Controllers/PenyimpananController.php
+++ app/Http/Controllers/PenyimpananController.php
@@ -9,19 +9,83 @@ use App\Models\Penyimpanan;
 class PenyimpananController extends Controller
 {
     public function index(){
+        $user = $request->session()->get('user');
+        
+        $user = User::where('id', $user->id)->first();
+
         $penyimpanan = Penyimpanan::all();
-        return view('welcome', ['penyimpanan'=>$penyimpanan]);
+        return view('welcome', compact('penyimpanan', 'user'));
+    }
+
+    public function create(Request $request)
+    {
+        $user = $request->session()->get('user');
+        
+        $user = User::where('id', $user->id)->first();
+        
+        return view('penyimpanan.create', compact('user'));
     }
 
     public function add(Request $req){
-        $penyimpanan = new Penyimpanan;
-        $penyimpanan->id = $req->id;
-        $penyimpanan->nama_barang = $req->nama_barang;
-        $penyimpanan->deskripsi = $req->deskripsi;
-        $penyimpanan->stok_tersedia = $req->stok_tersedia;
-        $penyimpanan->harga_satuan = $req->harga_satuan;
-        $penyimpanan->kategori = $req->kategori;
-        $penyimpanan->save();
-        return redirect()->back();
+        $validated = $request->validate([
+            'nama_barang' => 'required|max:50',
+            'deskripsi' => 'required',
+            'stok_tersedia' => 'required|numeric',
+            'harga_satuan' => 'required|numeric',
+            'kategori' => 'required',
+        ]);
+
+        Penyimpanan::create([
+            'nama_barang' => $request->nama_barang,
+            'deskripsi' => $request->deskripsi,
+            'stok_tersedia' => $request->stok_tersedia,
+            'harga_satuan' => $request->harga_satuan,
+            'kategori' => $request->kategori,
+        ]);
+
+        session()->flash('success', 'Data penyimpanan baru berhasil ditambahkan!');
+
+        return redirect()->route('index-penyimpanan');
+    }
+
+    public function edit(Request $request, Penyimpanan $penyimpananBarang)
+    {
+        $user = $request->session()->get('user');
+        
+        $user = User::where('id', $user->id)->first();
+
+        return view('penyimpanan.edit', compact('penyimpananBarang', 'user'));
+    }
+
+    public function update(Request $request, Penyimpanan $penyimpananBarang)
+    {
+        $validated = $request->validate([
+            'nama_barang' => 'required|max:50',
+            'deskripsi' => 'required',
+            'stok_tersedia' => 'required|numeric',
+            'harga_satuan' => 'required|numeric',
+            'kategori' => 'required',
+        ]);
+
+        $penyimpananBarang->update([
+            'nama_barang' => $request->nama_barang,
+            'deskripsi' => $request->deskripsi,
+            'stok_tersedia' => $request->stok_tersedia,
+            'harga_satuan' => $request->harga_satuan,
+            'kategori' => $request->kategori,
+        ]);
+        
+        session()->flash('success', 'Data penyimpanan berhasil diperbaharui!');
+
+        return redirect()->route('index-penyimpanan');
+    }
+
+    public function delete(Penyimpanan $penyimpananBarang)
+    {
+        $penyimpananBarang->delete();
+
+        session()->flash('success', 'Data penyimpanan berhasil dihapus!');
+
+        return redirect()->route('index-penyimpanan');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PenyimpananController;
+use App\Http\Controllers\UploadFileController;
+use App\Http\Middleware\Authentication;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PenyimpananController::class, 'index']);
-Route::post('/add', [PenyimpananController::class, 'add']);
+Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login-form');
+Route::post('/login', [AuthenticationController::class, 'login'])->name('login-post');
+Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+
+Route::get('/', [DashboardController::class, 'index'])->name('welcome-page')->middleware(Authentication::class);
+
+Route::get('/penyimpanan/', [PenyimpananController::class, 'index'])->name('index-penyimpanan')->middleware(Authentication::class);
+Route::get('/penyimpanan/create', [PenyimpananController::class, 'create'])->name('create-penyimpanan')->middleware(Authentication::class);
+Route::post('/penyimpanan/store', [PenyimpananController::class, 'store'])->name('store-penyimpanan')->middleware(Authentication::class);
+Route::get('/penyimpanan/edit/{penyimpananBarang}', [PenyimpananController::class, 'edit'])->name('edit-penyimpanan')->middleware(Authentication::class);
+Route::put('/penyimpanan/update/{penyimpananBarang}', [PenyimpananController::class, 'update'])->name('update-penyimpanan')->middleware(Authentication::class);
+Route::delete('/penyimpanan/delete/{penyimpananBarang}', [PenyimpananController::class, 'delete'])->name('delete-penyimpanan')->middleware(Authentication::class);
+
+Route::get('/profile', [UploadFileController::class, 'FormAvatar'])->name('profile')->middleware(Authentication::class);
+Route::put('/profile/avatar/{user}', [UploadFileController::class, 'store'])->name('profile-store')->middleware(Authentication::class);
